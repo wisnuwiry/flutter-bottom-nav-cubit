57e117c91d0d10f98864ef204c59a970833713af
masrobot <muhamadmashudiardiwinata@gmail.com>
2021-01-29 00:30:59 +0700

    refactoring code to latest cubit

--- lib/cubit/botttom_nav_cubit.dart
+++ lib/cubit/botttom_nav_cubit.dart
@@ -1,4 +1,4 @@
-import 'package:cubit/cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BottomNavCubit extends Cubit<int> {
   BottomNavCubit() : super(0);
@@ -6,6 +6,7 @@ class BottomNavCubit extends Cubit<int> {
   /// update index function to update the index onTap in BottomNavigationBar
   void updateIndex(int index) => emit(index);
 
+  /// for navigation button on single page
   void getHome() => emit(0);
   void getTasks() => emit(1);
   void getApps() => emit(2);
